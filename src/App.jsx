0a75b5dfd91e80af18ff5327206e3aafc36c0cfa
shakilkhadev <shakilkhanfv@gmail.com>
2025-04-13 14:46:35 +0600

    initail commit  - fetched data from api and mount them in componet , little work on responsible for mobile , more to work on

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+import Country from './component/country/country'
+
+function App() {
+
+  return (
+    <>
+
+      <div className="container">
+        <h1 className='title'>All Country of Asia</h1>
+      </div>  
+      <div>  
+      <Country/>
+           </div>
+    
+
+
+    </>
+  )
+}
+
+export default App
---
+++ src/component/country/Country.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+function Country() {
+  const [countries, setCountries] = useState([]);
+  const [i, setI] = useState(10);
+  const [loadItem, setLoadItem] = useState(true);
+
+  
+
+  // const [countryName, setCountryName] = useState();
+  // const [capital, setCapital] = useState();
+  // const [population, setPopulation] = useState();
+  // const [flag,setFlag] = useState();
+
+  useEffect(() => {
+    const data = fetch("https://restcountries.com/v3.1/region/asia");
+
+    data
+      .then((response) => {
+        return response.json();
+      })
+      .then((item) => {
+        const sortedItem =[...item].sort((a,b)=>a.name.common.localeCompare(b.name.common));
+        setCountries(sortedItem);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+     
+  }, [ ]);
+
+  
+
+   
+   
+  
+
+  function loadCountries() {
+
+    if (i < countries.length-10) {
+      setLoadItem(true);
+      
+    } else if (i >= countries.length-10) {
+      setLoadItem(false);
+    }
+    loadItem ? setI(i + 10) : "";
+   
+    
+    console.log(i);
+    
+  }
+
+  function renderCountries() {
+    return (
+      <>
+     
+        <div className="parentContainer">
+          <div className="flagContainer">
+            {countries.slice(0, i).map((item, index) => {
+              return (
+                <div className="flag fade-in" key={index}>
+                  <h1 key={index}>Country: {item.name.common} </h1>
+                  <img width={200} height={100} src={item.flags.svg} alt="" />
+                  <h3>Capital: {item.capital}</h3>
+                  <p>Population: {item.population}</p>{" "}
+                </div>
+              );
+            })}
+
+            <div className="loadMore">
+              <button
+                className={loadItem ? "loadMoreBtn" : "loadMoreBtn disable"}
+                onClick={() => loadCountries()}
+              >
+                Load More
+              </button>
+
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+
+  return renderCountries();
+}
+
+export default Country;
